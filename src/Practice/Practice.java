d8e0431983da2f187e330602109cb7083c8337b9
jitendra Lowanshi <jitendralowanshi3@gmail.com>
2024-12-17 14:27:26 +0530

    modified practice.java in practices

--- src/Practice/Practice.java
+++ src/Practice/Practice.java
@@ -5,8 +5,7 @@ import java.util.Scanner;
 public class Practice {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your decimal number ");
-        System.out.println("jitendra lowanshig");
+
         int num = sc.nextInt();
         String b = " ";
         int temp = num;
