27d9c834086e9f2576b7b82e0816d1b5160e507b
jitendra Lowanshi <jitendralowanshi3@gmail.com>
2024-12-17 14:33:49 +0530

    delet extra print lines in practice.java

--- src/Practice/Practice.java
+++ src/Practice/Practice.java
@@ -6,8 +6,6 @@ public class Practice {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter your decimal number ");
-        System.out.println("abcdefghigk jkl");
-        System.out.println("735483");
 
         int num = sc.nextInt();
         String b = " ";
